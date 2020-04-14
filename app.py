6b073a341a85575e0c5be9e793ff0c40e0fcb61e
Julio Carvajal B <soporte@creasyscr.com>
2020-04-13 19:31:10 -0600

    por nueva version PYTHON

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'hola'
 api = Api(app)
 
+@app.before_first_request
+def create_TABLES():
+    db.create_all()
+
 jwt = JWT(app, authenticate, identity)  #/auth
 
 api.add_resource(Store, '/store/<string:name>')
